91640e247a2ee39410a62959d425ac956f96e6b4
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-06-18 08:43:28 +0700

    first commit

---
+++ assets/js/default/client-zetachat.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".customer-logos").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
+  
+  
+  
+  
